b7d1588207ccf42addb004a9b91dd9311e239a91
bautista00 <bautistaquesada00@gmail.com>
2024-03-02 20:25:39 -0300

    Requerimiento 2 y 3 realizado, funcionando todo el main con goroutines

--- internal/tickets/tickets.go
+++ internal/tickets/tickets.go
@@ -3,7 +3,11 @@ package tickets
 import (
 	"encoding/csv"
 	"os"
+	"strconv"
+	"strings"
 )
+
+
 type Ticket struct {
 	
 	ID  int
@@ -16,6 +20,8 @@ type Ticket struct {
 }
 
 // Requerimiento 1
+// Una función que calcule cuántas personas viajan a un país determinado.
+
 func GetTotalTickets(destination string) (int, error) {
 	file, err := os.Open("tickets.csv")
 	
@@ -41,11 +47,91 @@ func GetTotalTickets(destination string) (int, error) {
 }
 
 // Requerimiento 2
-func GetMornings(time string) (int, error) {
+// Una o varias funciones que calculen cuántas personas 
+// viajan en madrugada (0 → 6),
+// mañana (7 → 12), tarde (13 → 19), y noche (20 → 23).
 
+func GetCountByPeriod(filePath string, time string) (int, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return 0, err
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	rows, err := reader.ReadAll()
+	if err != nil {
+		return 0, err
+	}
+
+	tickets := make([]Ticket, len(rows))
+	for i, row := range rows {
+		id, _ := strconv.Atoi(row[0])
+		price, _ := strconv.ParseFloat(row[5], 64)
+
+		tickets[i] = Ticket{
+			ID:          id,
+			Name:        row[1],
+			Email:       row[2],
+			Destination: row[3],
+			Time:        row[4],
+			Price:       price,
+		}
+	}
+
+	count := 0
+	for _, ticket := range tickets {
+		hour, err := strconv.Atoi(strings.Split(ticket.Time, ":")[0])
+		if err != nil {
+			return 0, err
+		}
+
+		switch time {
+		case "Madrugada":
+			if hour >= 0 && hour < 6 {
+				count++
+			}
+		case "Mañana":
+			if hour >= 7 && hour < 12 {
+				count++
+			}
+		case "Tarde":
+			if hour >= 13 && hour < 19 {
+				count++
+			}
+		case "Noche":
+			if hour >= 20 && hour <= 23 {
+				count++
+			}
+		}
+	}
+
+	return count, nil
 }
 
 // Requerimiento 3
-func AverageDestination(destination string, total int) (int, error) {
+// Calcular el porcentaje de personas que viajan a 
+// un país determinado en un día.
 
+func PercentageDestination(filePath string, destination string) (float64, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return 0, err
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	rows, err := reader.ReadAll()
+	if err != nil {
+		return 0, err
+	}
+	count := 0
+	for _, row := range rows {
+		if row[3] == destination{
+			count++
+		}
+	}
+	percentage := float64(count) / float64(1000) * 100
+
+	return percentage, nil
 }
--- main.go
+++ main.go
@@ -4,18 +4,49 @@ import (
 	"github.com/bootcamp-go/desafio-go-bases/internal/tickets"
 	// "encoding/csv"
 	"fmt"
+	"time"
 )
 
 func main() {
-	
-	go func(){
-		
-		total, err := tickets.GetTotalTickets("Brazil")
+
+	go func(pais string) {
+		total, err := tickets.GetTotalTickets(pais)
 		if err != nil {
-			fmt.Println(err)
+			fmt.Printf("Error en GetTotalTickets para %s: %v", pais, err)
 			return
 		}
+		fmt.Printf("Total tickets for %s: %d\n",pais,total)
+	}("Brazil")
+	
 
-		fmt.Printf("Total tickets for Brazil: %d\n", total)
-		}()
+	filePath := "tickets.csv"
+	times := []string{"Madrugada", "Mañana", "Tarde", "Noche"}
+	for _, time := range times {
+		go func(t string) {
+			count, err := tickets.GetCountByPeriod(filePath, t)
+			if err != nil {
+				fmt.Printf("Error en GetCountByPeriod para %s: %v\n", t, err)
+				return
+			}
+			fmt.Printf("Personas que viajan en %s: %d\n", t, count)
+		}(time)
+	}
+	go func(paisPorcentaje string) {
+        total, err := tickets.PercentageDestination(filePath,paisPorcentaje)
+        if err != nil {
+             fmt.Printf("Error en PercentageDestination para %s: %v", paisPorcentaje, err)
+            return
+        }
+         fmt.Printf("El porcentaje de personas que viajaron a %s es %.2f\n", paisPorcentaje, total)
+    }("China")
+
+
+
+	time.Sleep(2 * time.Second)
+
 }
+
+	
+
+
+
